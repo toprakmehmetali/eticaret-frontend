775694bce1b56ebbe693e3a0850f26afcf297ca0
Mehmet Ali TOPRAK <92664474+toprakmehmetali@users.noreply.github.com>
2022-06-14 21:12:50 +0300

    son

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponentsComponent } from './admin/admin-components/admin-components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponentsComponent
   ],
   imports: [
     BrowserModule,
